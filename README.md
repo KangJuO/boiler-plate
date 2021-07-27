6ef4d8cb4eff7ee6b2e9745cef21a9339ccf3fb3
KangJuO <juy5437@gmail.com>
2021-07-27 16:30:58 +0900

    add body-parser, nodemon module

---
+++ README.md
@@ -0,0 +1,1 @@
+# clone-coded-boiler-plate
--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 //install express module => can start with express function 
 //install mongoose module => can connect to mongDB
-const express = require('express')
-const app = express()
-const port = 3000
-const mongoose = require('mongoose')
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const { User } = require('./models/User');
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://KangJuO:13241324@boilerplate.z4gvj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
      useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false  
 }).then(() =>  console.log('MongDB Connected...'))
@@ -11,6 +18,18 @@ mongoose.connect('mongodb+srv://KangJuO:13241324@boilerplate.z4gvj.mongodb.net/m
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+  //회원가입시 필요한 정보들을 client에서 가져오면
+  //가져온 정보들을 db에 넣어준다.
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => { 
+    if(err) return res.json({success: false, err});
+    return res.status(200).json({success: true}); 
+  })
+
 })
 
 app.listen(port, () => {
