15c9d35dc957c78b2da6dc0cc1a1fa6e758d911a
KangJuO <juy5437@gmail.com>
2021-08-12 12:09:52 +0900

    feat : login, logout, join

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -2,8 +2,7 @@ import React, { useEffect } from 'react'
 import axios from 'axios'
 
 
-function LandingPage() {
-
+function LandingPage(props) {
 
     //랜딩에 들어오자마자 실행
     //그냥 요청하면 안됨 cors 정책을 따라야함
@@ -13,9 +12,27 @@ function LandingPage() {
         .then(res => console.log(res.data))
     }, [])
 
+    const onLogoutHandler = () => {
+        axios.get('/api/users/logout')
+            .then(response => {
+                if(response.data.success){
+                    props.history.push('/login')
+                } else {
+                    alert('Fail to Logout')
+                }
+            })
+    }
+
     return (
-        <div>
-            LandingPage
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center',
+            width: '100%', height: '100vh'
+        }}>
+            <h2>시작 페이지</h2>
+
+            <button onClick={onLogoutHandler}>
+                Logout
+            </button>
         </div>
     )
 }
